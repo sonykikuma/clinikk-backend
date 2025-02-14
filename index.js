a40d890232bed6d39ae5991d56de9062b39d5b87
sonykikuma <er.abhasoni@gmail.com>
2025-02-14 10:43:17 +0530

    removed undesired code from index

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "*"],
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -28,28 +28,15 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-//const file = "./videos/large.mov";for initial testing into cloudinary
+//const file = "./videos/small.mp4";for initial testing into cloudinary
 async function run() {
-  // for files less than 10 mb
-  try {
-    const result = await cloudinary.v2.uploader.upload(file, {
-      resource_type: "video",
-    });
-    console.log(`> Result:${result.secure_url}`);
-  } catch (error) {
-    console.log(error);
-  }
-}
-//for large videos
-async function run1() {
   try {
     const result = await new Promise((resolve, reject) => {
       cloudinary.v2.uploader.upload_large(
-        //files above 100mb needs upload_large
+        //files above 6mb needs upload_large
         file,
         {
           resource_type: "video",
-          //  transformation: [{ quality: "auto", fetch_format: "mp4" }],   test failed
         },
         (error, result) => {
           if (error) {
@@ -64,13 +51,13 @@ async function run1() {
     console.log(error);
   }
 }
-//run1();
+//run();
 
 //multer
-const storage = multer.memoryStorage(); // Store in memory instead of disk
+const storage = multer.memoryStorage();
 const upload = multer({
   storage,
-  limits: { fileSize: 100 * 1024 * 1024 }, // 100MB file limit
+  limits: { fileSize: 100 * 1024 * 1024 }, // here 100MB file limit
 });
 
 app.get("/", (req, res) => {
@@ -108,45 +95,6 @@ app.post("/uploadVideo", upload.single("video"), async (req, res) => {
     res.status(500).json({ error: "Upload failed", details: error.message });
   }
 });
-
-// app.post("/uploadVideo", upload.single("video"), async (req, res) => {
-//   try {
-//     if (!req.file) {
-//       return res.status(400).json({ error: "no file uploaded" });
-//     }
-
-//     const { title, description } = req.body;
-//     const result = await new Promise((resolve, reject) => {
-//       cloudinary.v2.uploader.upload_large(
-//         req.file.path,
-//         {
-//           resource_type: "video",
-//         },
-//         (error, result) => {
-//           if (error) {
-//             reject(error);
-//           }
-//           resolve(result);
-//         }
-//       );
-//     });
-//     fs.unlinkSync(req.file.path);
-
-//     const newVideo = new VideoModel({
-//       title,
-//       videoUrl: result.secure_url,
-//       description,
-//     });
-//     await newVideo.save();
-
-//     res.status(201).json({
-//       message: "video uploaded and saved successfully",
-//       video: newVideo,
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error: "upload failed", details: error.message });
-//   }
-// });
 
 //api to fetch all the videos saved in db
 app.get("/videos", async (req, res) => {
